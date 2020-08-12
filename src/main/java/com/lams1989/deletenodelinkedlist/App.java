cd955cf7bbff73e611dd2691adbf26dd510f4fe1
Luis Munoz <luisalejandromunozsierra@gmail.com>
2020-08-12 20:43:45 -0300

    first commit

---
+++ src/main/java/com/lams1989/deletenodelinkedlist/App.java
@@ -0,0 +1,13 @@
+package com.lams1989.deletenodelinkedlist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
